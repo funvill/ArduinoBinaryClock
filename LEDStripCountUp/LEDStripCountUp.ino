87b55128a3089c52f3d097941d990bbd92779b58
Steven Smethurst <steven@abluestar.com>
2015-09-19 01:25:42 -0700

    Added set time offset function.
    This function will set the current time in the application.

--- LEDStripCountUp/LEDStripCountUp.ino
+++ LEDStripCountUp/LEDStripCountUp.ino
@@ -2,7 +2,7 @@
 // Created by: Steven Smethurst (funvill) 
 // Created on: September 16, 2015 
 // Created for: https://goo.gl/YlIgWO
-// Last updated: September 16, 2015 
+// Last updated: September 19, 2015 
 // 
 // 
 
@@ -36,7 +36,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, LED_PIN, NEO_GRB + NEO_KHZ800);
 // 
 // The Arduino Pro Mini uses a ATmega328 chip. This chip is a 8 bit AVR. 
 // Because of this the size of a unsigned long is 4 BYTES or 32 bits. 
-uint32_t gTime ; 
+uint32_t gClockOffset ; 
 
 uint32_t gSecondColorOn, gSecondColorOff; 
 
@@ -59,83 +59,39 @@ void setup() {
   gSecondColorOff = strip.Color(0, 0, 0);
   
   // Reset the global time variable to a known value. 
+  // You can use this website to get the current Epoch time http://www.epochconverter.com/
   // 1442646883 = GMT: Sat, 19 Sep 2015 07:14:43 GMT || 9/19/2015, 12:14:43 AM GMT-7:00 DST
   // 1442874600 = GMT: Mon, 21 Sep 2015 22:30:00 GMT || 9/21/2015, 03:30:00 PM GMT-7:00 DST
-  gTime = 1442874600 ; 
+  gClockOffset = 0 ; 
 }
 
-// This function will print the value as a decimal number and in binary 
-// This can be useful for debuging what is happening in your application. 
-void debugPrintBinary( uint32_t value ) {
-  Serial.print("Value=[");
-  Serial.print( value, DEC );
-  Serial.print("] Binary=[");
-  Serial.print( value, BIN );
-  Serial.println("]");  
-}
 
-// Set all the LEDS to the same color. 
-void setAllLEDs( uint32_t c ){
-  // Loop thought all the LEDs and set the color of the LED to the same color. 
-  for( unsigned char pixelOffset = 0 ; pixelOffset < strip.numPixels() ; pixelOffset++ ) {
-    strip.setPixelColor(pixelOffset, c );
-  }
-}
-
-// This function is used to make sure all the LEDs in the strip are functioning correctly
-void testLEDStrip() {    
-  // Rainbow 
-  // Cycle thought the rainbow 5 times quickly. 
-  for( int rainbowOffset = 0 ; rainbowOffset < 256*5 ; rainbowOffset++ ) {
-    for( int pixelOffset = 0 ; pixelOffset < strip.numPixels() ; pixelOffset++ ) {
-      strip.setPixelColor(pixelOffset, Wheel(((pixelOffset * 256 / strip.numPixels()) + rainbowOffset) & 255));
-    }
-    strip.show(); 
-    delay(1); 
-  }
-  setAllLEDs( gSecondColorOff ); 
-}
-
-// This function will take a value and extract the bits from the value to be shown as pixels on the 
-// LED strip. 
-void UpdateLEDsFromValue( uint32_t value ) {
-  debugPrintBinary( value ); 
+void loop() {
+ 
+  // simpleCountUpClock(); 
+  // countUpBasedOnTimer(); 
+  fancyCountUpClock(); 
+  // CountDownTimer( 120 ); 
   
-  // Loop thought the value extracting the bits. If a bit is 1 then turn the pixel to the on color 
-  // If the bit is off then turn the pixel to the off color. 
-  
-  // sizeof() 
-  // The sizeof operator returns the number of bytes in a variable type, or the number of bytes 
-  // occupied by an array.
-  // Reference: https://www.arduino.cc/en/Reference/sizeof
-  
-  for( unsigned char pixelOffset = 0 ; pixelOffset <= (sizeof( uint32_t ) * 8) ; pixelOffset++ ) {
-    // bitRead()
-    // Reads a bit of a number. 
-    // Reference: https://www.arduino.cc/en/Reference/bitRead
-    
-    if( bitRead(value, pixelOffset) == 1 ) {
-      strip.setPixelColor(pixelOffset, gSecondColorOn ); // On 
-    } else {
-      strip.setPixelColor(pixelOffset, gSecondColorOff ); // Off
-    }
-  }
-  
-  // Show the updated LEDS.
-  strip.show();
-
+  CheckForSetTime();
 }
+
+
+// -------------------------------------------------------------------
+// Clock Codes 
+// -------------------------------------------------------------------
+
 
 // This is the simplest form of a count up clock. 
-// When the program starts the value of gTime will be set to zero. Every time this function is run the 
-// value of gTime will be incurmented by one. At the end of this function the program is delayed by one second. 
+// When the program starts the value of gClockOffset will be set to zero. Every time this function is run the 
+// value of gClockOffset will be incurmented by one. At the end of this function the program is delayed by one second. 
 // 
 // This function has a major flaw with it, can you guess what the flaw is? 
 void simpleCountUpClock(){
-  gTime++ ; // Each loop, incurment the timer. 
+  gClockOffset++ ; // Each loop, incurment the timer. 
   
-  // Display the gTime on the LEDs 
-  UpdateLEDsFromValue( gTime ); 
+  // Display the gClockOffset on the LEDs 
+  UpdateLEDsFromValue( gClockOffset ); 
   
   // Delay() 
   // Pauses the program for the amount of time (in miliseconds) 
@@ -161,7 +117,7 @@ void countUpBasedOnTimer(){
     previousMillis = currentMillis ; // Update the previouse time 
     
     // Update the LEDs 
-    UpdateLEDsFromValue( gTime + ( currentMillis / MS_PER_SECOND ) ); 
+    UpdateLEDsFromValue( gClockOffset + ( currentMillis / MS_PER_SECOND ) ); 
   }
 }
 
@@ -191,16 +147,14 @@ void fancyCountUpClock() {
     if( pixelMillisOffset > 0 ) { 
       strip.setPixelColor(pixelMillisOffset-1, gSecondColorOff ); 
     }
-
     
     // Update the seconed LEDs 
-    uint32_t currentTime = gTime + ( currentMillis / MS_PER_SECOND ) ; 
-    for( int pixelOffset = 0 ; pixelOffset < (sizeof( uint32_t ) * 8) ; pixelOffset++ ) {
+    uint32_t currentTime = gClockOffset + ( currentMillis / MS_PER_SECOND ) ; 
+    for( int pixelOffset = 0 ; pixelOffset < (sizeof( uint32_t ) * 8) && pixelOffset < strip.numPixels() ; pixelOffset++ ) {
       if( bitRead( currentTime, pixelOffset) == 1 ) {
         strip.setPixelColor(pixelOffset, Wheel( (255 / strip.numPixels()) * pixelOffset ) ); // On 
       } 
-    }
-    
+    }  
     
     // Show the updated LEDS.
     strip.show();
@@ -251,6 +205,73 @@ void CountDownTimer( uint32_t timeInSeconds ) {
 }
 
 
+// -------------------------------------------------------------------
+// Helper functions 
+// -------------------------------------------------------------------
+
+
+// This function will print the value as a decimal number and in binary 
+// This can be useful for debuging what is happening in your application. 
+void debugPrintBinary( uint32_t value ) {
+  Serial.print("Value=[");
+  Serial.print( value, DEC );
+  Serial.print("] Binary=[");
+  Serial.print( value, BIN );
+  Serial.println("]");  
+}
+
+// Set all the LEDS to the same color. 
+void setAllLEDs( uint32_t c ){
+  // Loop thought all the LEDs and set the color of the LED to the same color. 
+  for( unsigned char pixelOffset = 0 ; pixelOffset < strip.numPixels() ; pixelOffset++ ) {
+    strip.setPixelColor(pixelOffset, c );
+  }
+}
+
+// This function is used to make sure all the LEDs in the strip are functioning correctly
+void testLEDStrip() {    
+  // Rainbow 
+  // Cycle thought the rainbow 5 times quickly. 
+  for( int rainbowOffset = 0 ; rainbowOffset < 256*5 ; rainbowOffset++ ) {
+    for( int pixelOffset = 0 ; pixelOffset < strip.numPixels() ; pixelOffset++ ) {
+      strip.setPixelColor(pixelOffset, Wheel(((pixelOffset * 256 / strip.numPixels()) + rainbowOffset) & 255));
+    }
+    strip.show(); 
+    delay(1); 
+  }
+  setAllLEDs( gSecondColorOff ); 
+}
+
+// This function will take a value and extract the bits from the value to be shown as pixels on the 
+// LED strip. 
+void UpdateLEDsFromValue( uint32_t value ) {
+  debugPrintBinary( value ); 
+  
+  // Loop thought the value extracting the bits. If a bit is 1 then turn the pixel to the on color 
+  // If the bit is off then turn the pixel to the off color. 
+  
+  // sizeof() 
+  // The sizeof operator returns the number of bytes in a variable type, or the number of bytes 
+  // occupied by an array.
+  // Reference: https://www.arduino.cc/en/Reference/sizeof
+  
+  for( unsigned char pixelOffset = 0 ; pixelOffset <= (sizeof( uint32_t ) * 8) && pixelOffset < strip.numPixels() ; pixelOffset++ ) {
+    // bitRead()
+    // Reads a bit of a number. 
+    // Reference: https://www.arduino.cc/en/Reference/bitRead
+    
+    if( bitRead(value, pixelOffset) == 1 ) {
+      strip.setPixelColor(pixelOffset, gSecondColorOn ); // On 
+    } else {
+      strip.setPixelColor(pixelOffset, gSecondColorOff ); // Off
+    }
+  }
+  
+  // Show the updated LEDS.
+  strip.show();
+}
+
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
@@ -266,10 +287,49 @@ uint32_t Wheel(byte WheelPos) {
   }
 }
 
-void loop() {
- 
-  // simpleCountUpClock(); 
-  // countUpBasedOnTimer(); 
-  fancyCountUpClock(); 
-  // CountDownTimer( 120 ); 
+// Check the serial port for a "SetTime" command If the set 
+// time command is set then update gClockOffset with 
+// The new time. This will allow the user update the clock 
+// With the current time, without having to reprogram the 
+// Arduino. 
+void CheckForSetTime() {  
+  static String inString = "";  // string to hold input
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char
+      // and add it to the string:
+      inString += (char)inChar;
+    }
+    
+    // if you get a newline, print the string,
+    // then the string's value:
+    if (inChar == 'u') {
+      
+      // Things get a little weired here. 
+      // millis() counts up from the start of the application. So if we want to set the time 
+      // and the visual of the time is based off the offset of gClockOffset + millis(). Then we need 
+      // account for the time that has already passed since the application has started. 
+      // In this case we take our set time and remove the amount of time that has passed since 
+      // the application started. 
+      // 
+      // BUT there is a gotcha. If the time that is being set is less then the current running 
+      // time then we will overflow the variable. So we need to account for this as well. Since
+      // There is no good action to do when the gClockOffset overflows we set the gClockOffset to a zero. 
+      // 
+      
+      gClockOffset = inString.toInt() ; 
+      if( gClockOffset < (millis() / 1000) ) {
+        gClockOffset = 0 ; 
+      } else {
+        gClockOffset -= (millis() / 1000) ; 
+      }
+
+      Serial.println("Clock offset set");
+      debugPrintBinary( gClockOffset ); 
+
+      // clear the string for new input:
+      inString = "";
+    }
+  }
 }
